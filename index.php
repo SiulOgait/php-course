ac95af94a6dc155cad1b6788c362b75cd8ca9c3d
siulogait <luistsilva77@googlemail.com>
2021-02-16 16:18:28 +0000

    added php variables

--- index.php
+++ index.php
@@ -5,9 +5,14 @@
   </head>
   <body>
     <?php
+
+    $name = "PHP Store";
+    $credit = 1000;
     // Using two forward slashes we create comments in php.
     // Within the php tag, we can insert values into our HTML.
-    echo "<h1>Welcome Tiagolas! Your first great PHP website in Ubuntu</h1>";
+    echo "<h1>Welcome Tiagolas to ".$name."! Your first great PHP website in Ubuntu</h1>";
+
+    echo "<h2>You have a $".$credit." in your virtual wallet</h2>"
     ?>
   </body>
 </html>
