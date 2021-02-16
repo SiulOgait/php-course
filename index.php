2f955d543efb18e99ed75871e308b355bdfdfdc6
siulogait <luistsilva77@googlemail.com>
2021-02-16 16:33:48 +0000

    Added array variable

--- index.php
+++ index.php
@@ -12,7 +12,18 @@
     // Within the php tag, we can insert values into our HTML.
     echo "<h1>Welcome Tiagolas to ".$name."! Your first great PHP website in Ubuntu</h1>";
 
-    echo "<h2>You have a $".$credit." in your virtual wallet</h2>"
+    echo "<h2>You have a $".$credit." in your virtual wallet</h2>";
+
+        //$ArrayVariable['keyName'] = 'something'
+        $products['Computer'] = 750;
+        $products['Car'] = 15000;
+        $products['iPhone'] = 900;
+        $products['Printer'] = 150;
+
+        foreach($products as $key => $value){
+          echo "<p>The ".$key." costs ".$value."</p>";
+        }
+
     ?>
   </body>
 </html>
