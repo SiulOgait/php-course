7c72ed63f8353257d72038978fb32540cffd126e
siulogait <luistsilva77@googlemail.com>
2021-02-16 16:07:34 +0000

    Update index.php
    First commit

---
+++ index.php
@@ -0,0 +1,13 @@
+!DOCTYPE html>
+<html>
+  <head>
+    <title>PHP Store</title>
+  </head>
+  <body>
+    <?php
+    // Using two forward slashes we create comments in php.
+    // Within the php tag, we can insert values into our HTML.
+    echo "<h1>Welcome Tiagolas! Your first great PHP website in Ubuntu</h1>";
+    ?>
+  </body>
+</html>
